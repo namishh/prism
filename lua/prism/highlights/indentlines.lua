5922de39ceeacba27ee01f11c9475d321e168fcf
chadcat7 <namishpande2007@gmail.com>
2023-12-03 15:16:32 +0530

    0301

--- lua/prism/highlights/indentlines.lua
+++ lua/prism/highlights/indentlines.lua
@@ -1,7 +1,7 @@
 local colors = require("prism.themer"):getColors()
 
 return {
-  IblIndent = { fg = colors.color0, bg = colors.background },
-  ["@ibl.indent.char.1"] = { fg = colors.color0, bg = colors.background },
+  IblIndent = { fg = colors.color0, bg = "NONE" },
+  ["@ibl.indent.char.1"] = { fg = colors.color0, bg = "NONE" },
 
 }
