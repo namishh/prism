84d68ce2b854f3f9a8232b066e032184653b1476
Namish <namishpande2007@gmail.com>
2023-12-26 15:30:19 +0530

    prism

---
+++ lua/prism/schemes/swamp.lua
@@ -0,0 +1,27 @@
+-- theme by maindak, here is his orginal theme https://github.com/masroof-maindak/swamp.nvim
+return {
+  name = "swamp",
+  background = "#201c14",
+  foreground = "#ebe0bb",
+  cursorline = "#231f16",
+  comment = "#8b815e",
+  darker = "#18150f",
+  cursor = "#ebe0bb",
+  black = "#1d1a11", -- useful when background is transparent
+  color0 = "#242115",
+  color1 = "#ad335b",
+  color2 = "#7c9151",
+  color3 = "#db930d",
+  color4 = "#db930d",
+  color5 = "#b26a89",
+  color6 = "#cb7277",
+  color7 = "#ebe0bb",
+  color8 = "#292518",
+  color9 = "#c73667",
+  color10 = "#8da656",
+  color12 = "#dfb03a",
+  color11 = "#dfb03a",
+  color13 = "#b06c89",
+  color14 = "#dd8e92",
+  color15 = "#e1d6b3",
+}
