85ef66033df94ff9543d9ad4d45db61233734aec
Namish <namishpande2007@gmail.com>
2024-01-12 11:11:51 +0530

    fix indentline

--- lua/prism/highlights/indentlines.lua
+++ lua/prism/highlights/indentlines.lua
@@ -1,7 +1,7 @@
 local colors = require("prism.themer"):getColors()
 
 return {
-  IblIndent = { fg = colors.color0, bg = "NONE" },
-  ["@ibl.indent.char.1"] = { fg = colors.color0, bg = "NONE" },
+  IblIndent = { fg = colors.color0 },
+  ["@ibl.indent.char.1"] = { fg = colors.color0 },
 
 }
